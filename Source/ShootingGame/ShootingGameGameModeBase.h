af12002b63bfd8697db857b520d2847828224753
wsq94317 <wsq94317@vip.qq.com>
2023-09-17 14:10:58 +1000

    Create Empty Project

---
+++ Source/ShootingGame/ShootingGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootingGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGGAME_API AShootingGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
